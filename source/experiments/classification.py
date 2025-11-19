e379b4dbcebce6bb3eb1b054b70e05b8d571e67f
Juxh01 <julius@DESKTOP-LDQ7B16.localdomain>
2025-11-19 17:51:47 +0100

    Add classification experiment

---
+++ source/experiments/classification.py
@@ -0,0 +1,45 @@
+import os
+
+import hydra
+import torch
+import wandb
+from omegaconf import DictConfig, OmegaConf
+
+from source.setup import setup_classification
+from source.train import train
+
+
+@hydra.main(config_path="./configs", config_name="classification", version_base="1.1")
+def main(cfg: DictConfig) -> None:
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    config_dict = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+    # Setup the classification environment
+    model, train_loader, val_loader, train_sampler, optimizer, scheduler = (
+        setup_classification(device=device, config=config_dict)
+    )
+
+    rank = int(os.environ["RANK"])
+    run = None
+    if rank == 0:
+        # Initialize Weights & Biases logging
+        run = wandb.init(
+            project=cfg.general.wandb_project,
+            config=config_dict,
+            name=cfg.general.experiment_name,
+        )
+
+    train(
+        device=device,
+        config=config_dict,
+        model=model,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        train_sampler=train_sampler,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        run=run,
+    )
+
+
+if __name__ == "__main__":
+    main()
