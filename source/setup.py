253a56ff5eeeacf0bac2b8bd9c056f9932acb9b0
Juxh01 <julius@DESKTOP-LDQ7B16.localdomain>
2025-11-19 15:53:50 +0100

    Remove errors from setup.py and add first config

--- source/setup.py
+++ source/setup.py
@@ -10,11 +10,13 @@ import numpy as np
 import torch
 from detonation import (
     DeMoReplicator,
+    FullReplicator,
     NoReplicator,
     Optimizers,
     RandomReplicator,
     prepare_detonation,
 )
+from torch import distributed as dist
 from torch.distributed.fsdp import (
     MixedPrecision,
 )
@@ -22,7 +24,7 @@ from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
-from torchvision import datasets
+from torchvision import datasets, tv_tensors
 from torchvision.transforms import v2 as T
 from transformers import (
     DPTConfig,
@@ -58,8 +60,15 @@ def setup_distributed_training(
     seed: int,
     config: Dict[str, Any],
 ):
+    # Initialize the process group for distributed training
+    if not dist.is_initialized():
+        dist.init_process_group(backend="nccl")
+
+    # Set the device for the current process
     if device == "cuda":
         torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
+
+    optim_config = config["optimizer"]
 
     auto_wrap_policy = functools.partial(
         transformer_auto_wrap_policy, transformer_layer_cls=transformer_layer_cls
@@ -73,20 +82,26 @@ def setup_distributed_training(
         if torch.cuda.is_bf16_supported()
         else None
     )
-    if config["repl"] == "deto-demo":
+
+    if mixed_precision is None:
+        print("Mixed precision with bfloat16 is not supported on this device.")
+
+    if optim_config["repl"] == "deto-demo":
         replicator = DeMoReplicator(
-            compression_topk=config["compression_topk"],
-            compression_chunk=config["compression_chunk"],
+            compression_topk=optim_config["compression_topk"],
+            compression_chunk=optim_config["compression_chunk"],
         )
-    elif config["repl"] == "deto-random":
+    elif optim_config["repl"] == "deto-random":
         replicator = RandomReplicator(
-            compression_rate=config["compression_rate"],
+            compression_rate=optim_config["compression_rate"],
             seed=seed,
         )
+    elif optim_config["repl"] == "deto-full":
+        replicator = FullReplicator()
     else:
         replicator = NoReplicator()
         raise NotImplementedError("NoReplicator is not wanted yet.")
-    opt_enum = Optimizers(config["optimizer_str"].lower())
+    opt_enum = Optimizers(optim_config["optimizer_str"].lower())
     model, optimizer = prepare_detonation(
         model,
         opt_enum,
@@ -95,14 +110,14 @@ def setup_distributed_training(
             "auto_wrap_policy": auto_wrap_policy,
             "mixed_precision": mixed_precision,
         },
-        replicate_every=config["replicate_every"],
-        skip_every=config["skip_every"],
-        sharding_group_size=config["shards"],
+        replicate_every=optim_config["replicate_every"],
+        skip_every=optim_config["skip_every"],
+        sharding_group_size=optim_config["shards"],
     )
     optim = optimizer._optimizer if hasattr(optimizer, "_optimizer") else optimizer
     for param_group in optim.param_groups:
-        param_group["lr"] = config["lr"]
-    scheduler = StepLR(optim, step_size=1, gamma=config["lr_gamma"])
+        param_group["lr"] = optim_config["lr"]
+    scheduler = StepLR(optim, step_size=1, gamma=optim_config["lr_gamma"])
 
     return optimizer, scheduler
 
@@ -131,22 +146,41 @@ def setup_classification(
         pooler_act="tanh",
         num_labels=100,
     )
-
-    model = ViTForImageClassification(vit_base_config)
+    backbone_name = config["general"].get("backbone_name", None)
+    if backbone_name:
+        print(f"Initializing ViT with pretrained backbone: {backbone_name}")
+        model = ViTForImageClassification.from_pretrained(
+            backbone_name, num_labels=100, image_size=224, ignore_mismatched_sizes=True
+        )
+    else:
+        print("Initializing ViT from scratch (Random Weights)")
+        model = ViTForImageClassification(vit_base_config)
     model.to(device)
 
     train_transforms = T.Compose(
         [
             T.RandomResizedCrop(size=(224, 224), scale=(0.5, 2.0)),
             T.RandomHorizontalFlip(),
-            T.ToDtype(torch.float32, scale=True),
+            T.ToDtype(
+                dtype={
+                    tv_tensors.Image: torch.float32,
+                    "others": None,
+                },
+                scale=True,
+            ),
             T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
     val_transforms = T.Compose(
         [
             T.Resize(size=(224, 224)),
-            T.ToDtype(torch.float32, scale=True),
+            T.ToDtype(
+                dtype={
+                    tv_tensors.Image: torch.float32,
+                    "others": None,
+                },
+                scale=True,
+            ),
             T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
@@ -171,14 +205,18 @@ def setup_classification(
         train_dataset,
         batch_size=batch_size,
         sampler=train_sampler,
-        num_workers=4,
+        num_workers=int(
+            os.environ.get("SLURM_CPUS_PER_TASK", 4)
+        ),  # TODO: dynamic value?
         shuffle=False,  # Shuffle is done by sampler
     )
     val_loader = DataLoader(
         val_dataset,
         batch_size=batch_size,
         sampler=val_sampler,
-        num_workers=4,
+        num_workers=int(
+            os.environ.get("SLURM_CPUS_PER_TASK", 4)
+        ),  # TODO: dynamic value?
         shuffle=False,
     )
 
@@ -215,6 +253,7 @@ def setup_segmentation(batch_size: int, seed: int, device: str, config: Dict[str
         is_hybrid=False,  # ViT-Base Backbone
         backbone_out_indices=[2, 5, 8, 11],  # Like in DPT-Paper
         readout_type="project",  # Default in DPT-Paper
+        num_labels=21,  # VOC has 21 classes
         # --- Decoder Configuration (default configuration) ---
         reassemble_factors=[4, 2, 1, 0.5],
         neck_hidden_sizes=[96, 192, 384, 768],
@@ -228,7 +267,7 @@ def setup_segmentation(batch_size: int, seed: int, device: str, config: Dict[str
         auxiliary_loss_weight=0.4,
         semantic_loss_ignore_index=255,
         semantic_classifier_dropout=0.1,
-        # --- Backbone-Configuration (no pretrained version) ---s
+        # --- Backbone-Configuration (no pretrained version) ---
         backbone_config=None,
         backbone=None,
         use_pretrained_backbone=False,
@@ -238,6 +277,13 @@ def setup_segmentation(batch_size: int, seed: int, device: str, config: Dict[str
         pooler_output_size=None,  # Defaults to hidden_size
         pooler_act="tanh",
     )
+    backbone_name = config["general"].get("backbone_name", None)
+    if backbone_name:
+        print(f"Initializing DPT with pretrained backbone: {backbone_name}")
+        dpt_base_config.use_pretrained_backbone = True
+        dpt_base_config.backbone = backbone_name
+    else:
+        print("Initializing DPT from scratch (Random Weights)")
 
     model = DPTForSemanticSegmentation(dpt_base_config)
     model.to(device)
@@ -246,14 +292,28 @@ def setup_segmentation(batch_size: int, seed: int, device: str, config: Dict[str
         [
             T.RandomResizedCrop(size=(384, 384), scale=(0.5, 2.0)),
             T.RandomHorizontalFlip(),
-            T.ToDtype(torch.float32, scale=True),
+            T.ToDtype(
+                dtype={
+                    tv_tensors.Image: torch.float32,
+                    tv_tensors.Mask: torch.int64,
+                    "others": None,
+                },
+                scale=True,
+            ),
             T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
     val_transforms = T.Compose(
         [
             T.Resize(size=(384, 384)),
-            T.ToDtype(torch.float32, scale=True),
+            T.ToDtype(
+                dtype={
+                    tv_tensors.Image: torch.float32,
+                    tv_tensors.Mask: torch.int64,
+                    "others": None,
+                },
+                scale=True,
+            ),
             T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ],
     )
@@ -282,14 +342,18 @@ def setup_segmentation(batch_size: int, seed: int, device: str, config: Dict[str
         train_dataset,
         batch_size=batch_size,
         sampler=train_sampler,
-        num_workers=4,
+        num_workers=int(
+            os.environ.get("SLURM_CPUS_PER_TASK", 4)
+        ),  # TODO: dynamic value?
         shuffle=False,  # Shuffle is done by sampler
     )
     val_loader = DataLoader(
         val_dataset,
         batch_size=batch_size,
         sampler=val_sampler,
-        num_workers=4,
+        num_workers=int(
+            os.environ.get("SLURM_CPUS_PER_TASK", 4)
+        ),  # TODO: dynamic value?
         shuffle=False,
     )
     optimizer, scheduler = setup_distributed_training(
